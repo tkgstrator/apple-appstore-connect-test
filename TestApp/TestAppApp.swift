7c43cb1e045fa606019395e00c26fe12233984f6
tkgstrator <nasawake.am@gmail.com>
2024-02-26 10:13:41 +0900

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by smorimoto on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
